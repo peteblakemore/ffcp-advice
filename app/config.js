09458eb090fd7a5bcb1ac511d8846bf11d78450a
Pete Blakemore <contact@peteblakemore.co.uk>
2022-03-14 15:50:42 +0000

    Set up index
    Index page created to include links to start pages of UP1 services.

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Information for farmers',
 
   // Default port that prototype runs on
   port: '3000',
